34a68982218d84df499c21d78a6a92651102eaf2
Andrii Krinskiy <andrewkrinskiy@gmail.com>
2020-05-23 18:44:10 +0300

    first commit

---
+++ src/components/header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="header">
+    <div class="header__title">Breweries</div>
+  </header>
+</template>>
+
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  align-items: center;
+  background-color: #ccc;
+  padding: 20px 0px;
+}
+</style>
